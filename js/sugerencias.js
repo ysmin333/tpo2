1a2f6ffbf3eafebfaf77bf6d997bc3b522ce52f5
Delfi <delfidriussi@gmail.com>
2023-05-16 16:01:20 -0300

    formulario

--- js/sugerencias.js
+++ js/sugerencias.js
@@ -3,3 +3,8 @@
 
 var error = document.getElementById ('error');
 error.style.color = 'red';
+
+const nombreIngresado = getElementById("name");
+const emailIngresado = getElementById("email");
+const boton = getElementById("boton");
+const form = getElementById("form");
